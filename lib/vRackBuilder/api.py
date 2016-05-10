a30c94ed1ccb91bbfe7d90d09901ce2c71ee5a52
helloarys <helloarys@gmail.com>
2016-05-10 17:17:36 +0800

    Change the ova getting url so it can support new vcompute nodes.

--- lib/vRackBuilder/api.py
+++ lib/vRackBuilder/api.py
@@ -20,7 +20,7 @@ from pyVmomi import vim
 import tasks
 
 IMAGE_PATH = 'ova/'
-URL_LINK = 'https://gpsbuildfarm.corp.emc.com/view/HW_Simulation/job/HW_Simulation_%s/%s/artifact/ova_artifact/%s'
+URL_LINK = 'http://10.32.136.39:8080/view/OVA%20build/job/%s/%s/artifact/tools/ova_builder/%s'
 
 def esxi_get_datastores(host, usr, pwd):
     """ Get all datastore information of 1 ESXi. """
